1e4dbb5dcec206818c4b9f8e04de070f870b8f69
anna-podobrii <anutik268@gmail.com>
2022-01-28 12:49:17 +0200

    Initial Commit

---
+++ MoodProject/MoodProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodProjectApp.swift
+//  MoodProject
+//
+//  Created by Anna Podobrii on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoodProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
